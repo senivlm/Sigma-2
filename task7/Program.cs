873a34270b4e4203f3a4c8e56288d53969a8150a
senivlm <91743906+senivlm@users.noreply.github.com>
2022-06-20 18:27:19 +0300

    Update Program.cs

--- task7/Program.cs
+++ task7/Program.cs
@@ -3,7 +3,7 @@
     public class Program
     {
         static void Main(string[] args)
-        {
+        {//8 завдання не знайшла!
 
             Storage s = new Storage();
             s.ParseFromFile();
